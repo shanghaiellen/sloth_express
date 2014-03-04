9f32527453883e88bdcf35199338a06e4d58900c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-04 13:20:37 -0800

    makes external api call

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,34 +12,20 @@ class Order < ActiveRecord::Base
 
 
   def get_estimate(zip)
-    estimate = {order:  { packages: [],
-                          origin: {zip: 98101},
-                          destination: {country: 'US',
-                                        zip: zip}
-                        }
-                }
+    estimate_params = {order:  {packages: [],
+                                origin: { country: 'US',
+                                          zip: 98101},
+                                destination: {country: 'US',
+                                              zip: zip}
+                              }
+                      }
     self.order_items.each do |item|
-      estimate[:order][:packages] << {weight: item.product.weight, 
+      estimate_params[:order][:packages] << {weight: item.product.weight, 
                                       dimensions: item.product.dimension_string,
                                       units: "imperial"}
     end
-    # uri = URI.parse("http://example.com/search")
-
-    # # Shortcut
-    # response = Net::HTTP.post_form(uri, {"q" => estimate })
-
-    # # Full control
-    # http = Net::HTTP.new(uri.host, uri.port)
-
-    # request = Net::HTTP::Post.new(uri.request_uri)
-    # request.set_form_data({"q" => "My query", "per_page" => "50"})
-
-    # response = http.request(request)
-
-    HTTParty.post('http://localhost:3000/get_estimate.json', 
-                  body: estimate.to_json, 
-                  headers: {'Content-Type' => 'application/json'})
-
+    response ||= HTTParty.post("http://localhost:3000/shipping_estimate.json", body: estimate_params)
+    response.parsed_response
   end
 
 end
