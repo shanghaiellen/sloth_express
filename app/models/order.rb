a02e234256f4061b4cc8f48838d008ee3cb055ab
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-08 21:42:06 -0800

    style changes as per rubocop

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,54 +8,51 @@ class Order < ActiveRecord::Base
   validate :order_items, presence: true
 
   def cart_size
-   order_items.sum(:quantity)
+    order_items.sum(:quantity)
   end
 
   def estimate_params(zip)
-    @estimate_params ||= {"order" =>  {"packages" => [],
-                                      "origin" => { "country" => 'US',
-                                                    "zip" => "98101"},
-                                                    "destination" => {"country" => 'US',
-                                                                      "zip"=> zip.to_s}
+    @estimate_params ||= { 'order' => { 'packages' => [],
+                                        'origin' => { 'country' => 'US',
+                                                      'zip' => '98101' },
+                                        'destination' =>  { 'country' => 'US',
+                                                            'zip' => zip.to_s }
                                       }
                           }
 
-    self.order_items.each do |item|
+    order_items.each do |item|
       item.quantity.times do
-        @estimate_params["order"]["packages"] << {"weight" => item.product.weight.to_s, 
-                                                  "dimensions" => item.product.dimension_string,
-                                                  "units" => "imperial"}
+        @estimate_params['order']['packages'] <<  { 'weight' => item.product.weight.to_s,
+                                                    'dimensions' => item.product.dimension_string,
+                                                    'units' =>  'imperial' }
       end
     end
   end
 
   def get_estimate(zip)
     estimate_params(zip)
-    @method = "POST"
+    @method = 'POST'
     @path = '/shipping_estimate.json'
-    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", 
-      body: @estimate_params,
-      headers: headers)
+    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", body: @estimate_params,
+                                                                           headers: headers)
     response.parsed_response
   end
 
   def get_cheapest(zip)
     estimate_params(zip)
-    @method = "POST"
+    @method = 'POST'
     @path = '/get_cheapest.json'
-    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", 
-      body: @estimate_params,
-      headers: headers)
+    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", body: @estimate_params,
+                                                                           headers: headers)
     response.parsed_response[0..4]
   end
 
   def get_fastest(zip)
     estimate_params(zip)
-    @method = "POST"
-    @path = "/get_fastest.json"
-    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", 
-      body: @estimate_params,
-      headers: headers)
+    @method = 'POST'
+    @path = '/get_fastest.json'
+    response ||= HTTParty.post("http://sloth-ship.herokuapp.com/#{@path}", body: @estimate_params,
+                                                                           headers: headers)
     response.parsed_response[0..4]
   end
 
@@ -63,9 +60,8 @@ class Order < ActiveRecord::Base
     time = Time.now.to_i.to_s
     # @key = ENV["API_KEY"]
     {
-      "REQUEST_TIME" => time,
-      "REQUEST_SIGNATURE" => ServiceAuthentication.new('testkey', @estimate_params, @path, @method, time).sign
+      'REQUEST_TIME' => time,
+      'REQUEST_SIGNATURE' => ServiceAuthentication.new('testkey', @estimate_params, @path, @method, time).sign
     }
   end
-
 end
